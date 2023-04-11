156d03ae1cdc141a6e6f2722ddc62d403d320513
admin <admin@admins-MacBook-Pro-2.local>
2023-04-11 18:02:11 +0530

    testing sonar cloud

--- components/Authentication/githubButton.js
+++ components/Authentication/githubButton.js
@@ -1,13 +1,13 @@
-import { useRouter } from "next/router";
+// import { useRouter } from "next/router";
 
-export default function GitButton () {
-    const router = useRouter();
+// export default function GitButton () {
+//     const router = useRouter();
 
-    const handleLogin = () => {
-        const CLIENT_ID = "ae594a90cd12119c8821";
-        const CLIENT_SECRET = "2cc3a03eb03d857f94c7a24180bfaaf59189cb6a";
+//     const handleLogin = () => {
+//         const CLIENT_ID = "ae594a90cd12119c8821";
+//         const CLIENT_SECRET = "2cc3a03eb03d857f94c7a24180bfaaf59189cb6a";
 
-        const scope = "user"
-    }
-}
+//         const scope = "user"
+//     }
+// }
 
